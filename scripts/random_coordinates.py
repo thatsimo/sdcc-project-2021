bf51102d540ab622a126680d2db08aabf2f3a403
Simone Squillace <simonewe99@gmail.com>
2022-05-19 20:44:51 +0200

    fix index-template for geo_point

--- scripts/random_coordinates.py
+++ scripts/random_coordinates.py
@@ -10,17 +10,19 @@ def add_location(boundaries_file, data_file):
         ".json", "") + "_USA.json", 'w')
 
     poly = shape(geojson_file['features']
-                 [238]['geometry'])
+                     [238]['geometry'])
     min_x, min_y, max_x, max_y = poly.bounds
 
     for line in input_file.readlines():
         entry = json.loads(line)
-
-        random_point = Point(
-            [random.uniform(min_x, max_x), random.uniform(min_y, max_y)])
-        entry['coordinates'] = {
-            'lat': random_point.y, 'lon': random_point.x}
-        print(random_point)
+        while True:
+            random_point = Point(
+                [random.uniform(min_x, max_x), random.uniform(min_y, max_y)])
+            if (random_point.within(poly)):
+                entry['coordinates'] = {
+                    'lat': random_point.y, 'lon': random_point.x}
+                print(random_point)
+                break
         output_file.write(json.dumps(entry) + "\n")
 
 
